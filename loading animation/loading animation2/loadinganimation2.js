0d571c6209214e70df8258dd8085a7d0e29a7749
SkuLL <102400540+ProjectSkuLL@users.noreply.github.com>
2022-10-28 11:31:51 +0530

    Add files via upload

---
+++ loading animation/loading animation2/loadinganimation2.js
@@ -0,0 +1,11 @@
+// js for loading animationn fade effect
+// it can be used when you wanna use the animation as a preloader.
+
+/*code starts here*/
+
+// setTimeout(function timeout(){
+//     document.getElementById("timeout").style.display = "none";
+// }, 2000);
+// timeouu();
+
+/*code ends here*/
